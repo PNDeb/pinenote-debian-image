532b52612e0427bc29d2531acc193648e5d56f6f
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2022-10-31 10:31:20 +0100

    update mutter and mesa to v20221031 compiles

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -6,10 +6,10 @@ cd overlays/custom_debs/
 test -d mutter && rm -r mutter
 mkdir mutter && cd mutter
 
-wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20221017/mutter.zip
+wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20221031/mutter.zip
 unzip mutter.zip *.deb
-rm *.dbgsym*.deb
-rm *.dev*.deb
+rm *dbgsym*.deb
+rm *dev*.deb
 rm mutter.zip
 cd ${pwd}
 
@@ -17,11 +17,11 @@ cd overlays/custom_debs/
 test -d mesa && rm -r mesa
 mkdir mesa && cd mesa
 
-wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/20221019/mesa_arm64_pinenote.zip
+wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v20221031/mesa_arm64_pinenote.zip
 
 unzip mesa_arm64_pinenote.zip *.deb
-rm *.dbgsym*.deb
-rm *.dev*.deb
+rm *dbgsym*.deb
+rm *dev*.deb
 rm mesa-opencl-icd*_arm64.deb
 rm mesa_arm64_pinenote.zip
 cd ${pwd}
