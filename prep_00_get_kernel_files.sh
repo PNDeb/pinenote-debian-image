026e75ba2167ee731b1378bc912effe98761bdb2
Maximilian Weigand <mweigand@mweigand.net>
2023-01-01 15:55:35 +0100

    prep kernel: we only need the .deb file

--- prep_00_get_kernel_files.sh
+++ prep_00_get_kernel_files.sh
@@ -6,12 +6,14 @@ mkdir tmp_kernel && cd tmp_kernel
 wget https://github.com/m-weigand/linux/releases/download/v20221207/pinenote_kernel_modules_dtb.zip
 unzip pinenote_kernel_modules_dtb.zip
 
-test -e overlays/modules/kernel-modules.tar.gz && rm overlays/modules/kernel-modules.tar.gz
+# test -e overlays/modules/kernel-modules.tar.gz && rm overlays/modules/kernel-modules.tar.gz
 
-# image and dtb
-cp rk3566-pinenote-v1.2.dtb ../overlays/boot/
-cp Image ../overlays/boot/
-cp modules.tar.gz ../overlays/modules/kernel-modules.tar.gz
+# # image and dtb
+# cp rk3566-pinenote-v1.2.dtb ../overlays/boot/
+# cp Image ../overlays/boot/
+# cp modules.tar.gz ../overlays/modules/kernel-modules.tar.gz
+
+cp linux-image*no_compression.deb ../overlays/kernel/
 
 cd ..
 rm -r tmp_kernel
