0c0d727c54ecfad676499e7bf68a1a06c1172122
Maximilian Weigand <mweigand@mweigand.net>
2023-01-03 12:09:12 +0100

    when toggling bootable-flags, use the legacy_boot instead of boot flag in parted. This makes it consistent with the bootable-flag of rkdeveloptools

--- overlays/root/toggle_partitions.sh
+++ overlays/root/toggle_partitions.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # switch the bootable-flag between partitions 17 and 18 using parted
 
-
-active_partition=$(parted /dev/mmcblk0 print | grep -e "^17" -e "^18" | nl | grep "boot, esp" | tr -d ' ' | cut -f 1)
+# active_partition=$(parted /dev/mmcblk0 print | grep -e "^17" -e "^18" | nl | grep "boot, esp" | tr -d ' ' | cut -f 1)
+active_partition=$(parted /dev/mmcblk0 print | grep -e "^17" -e "^18" | nl | grep "legacy_boot" | tr -d ' ' | cut -f 1)
 echo ${active_partition}
 
 if [ ${active_partition} -eq 1 ]; then
 	echo "Partition 17 active, toggling to partition 18"
-	parted /dev/mmcblk0 set 18 boot on
-	parted /dev/mmcblk0 set 17 boot off
+	parted /dev/mmcblk0 set 18 legacy_boot on
+	parted /dev/mmcblk0 set 17 legacy_boot off
 else
 	echo "Partition 18 active, toggling to partition 17"
-	parted /dev/mmcblk0 set 17 boot on
-	parted /dev/mmcblk0 set 18 boot off
+	parted /dev/mmcblk0 set 17 legacy_boot on
+	parted /dev/mmcblk0 set 18 legacy_boot off
 fi
