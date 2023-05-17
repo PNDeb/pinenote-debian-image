fa8e4701d723dd8d1df5fed3d20897ae70666bdb
Maximilian Weigand <mweigand@mweigand.net>
2023-05-17 09:56:02 +0200

    update external packages: mutter: 43.4-1 mesa: 22.3.6-1+deb12u1 koreader 2023.04; also add (temporary) firmware_brcm80211 package that fixes some wifi issues

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -8,7 +8,8 @@ mkdir mutter && cd mutter
 
 # wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20221231/mutter.zip
 # wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230317/mutter.zip
-wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230428/mutter.zip
+# wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230428/mutter.zip
+wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230517/mutter.zip
 unzip mutter.zip *.deb
 rm *dbgsym*.deb
 rm *dev*.deb
@@ -20,8 +21,8 @@ cd overlays/custom_debs/
 test -d mesa && rm -r mesa
 mkdir mesa && cd mesa
 
-wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v22.3.3-1_v1/mesa_arm64_pinenote.zip
-
+# wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v22.3.3-1_v1/mesa_arm64_pinenote.zip
+wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v20230517/mesa_arm64_pinenote.zip
 unzip mesa_arm64_pinenote.zip *.deb
 rm *dbgsym*.deb
 rm *dev*.deb
@@ -33,7 +34,8 @@ cd ${pwd}
 # koreader
 cd overlays/custom_debs/
 rm koreader*.deb*
-wget https://github.com/koreader/koreader/releases/download/v2022.10/koreader-2022.10-arm64.deb
+# wget https://github.com/koreader/koreader/releases/download/v2022.10/koreader-2022.10-arm64.deb
+wget https://github.com/koreader/koreader/releases/download/v2023.04/koreader-2023.04-arm64.deb
 cd ${pwd}
 
 # evsieve
@@ -53,4 +55,11 @@ cd "${pwd}"
 cd overlays/custom_debs
 rm pinenote_dbus_service*.deb
 wget https://github.com/m-weigand/pinenote_dbus_service/releases/download/v20221207/pinenote_dbus_service_0.1.0_arm64.deb
+cd "${pwd}"
+
+# 2023.05.17: (temporarily) fixed brcm firmware
+# https://salsa.debian.org/diederik/firmware-nonfree/-/jobs/4221052/artifacts/browse/debian/output/
+cd overlays/custom_debs
+test -e firmware-brcm80211_20230310-1~exp3+salsaci_all.deb && rm firmware-brcm80211_20230310-1~exp3+salsaci_all.deb
+wget https://salsa.debian.org/diederik/firmware-nonfree/-/jobs/4221052/artifacts/raw/debian/output/firmware-brcm80211_20230310-1~exp3+salsaci_all.deb?inline=false -O firmware-brcm80211_20230310-1~exp3+salsaci_all.deb
 cd "${pwd}"
