885302879a06a0235f532626cfb31575f78d6085
Maximilian Weigand <mweigand@mweigand.net>
2023-06-03 20:25:11 +0200

    Switch to kernel 6.3-v2-pinenote

--- prep_00_get_kernel_files.sh
+++ prep_00_get_kernel_files.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env sh
 
-test -d tmp_kernel && rm -r tmp_kernel
-mkdir tmp_kernel && cd tmp_kernel
-# wget https://github.com/m-weigand/linux/releases/download/20221013/pinenote_kernel_modules_dtb.zip
-#wget https://github.com/m-weigand/linux/releases/download/v20221207/pinenote_kernel_modules_dtb.zip
-wget https://github.com/m-weigand/linux/releases/download/v20230119/pinenote_kernel_modules_dtb.zip
-unzip pinenote_kernel_modules_dtb.zip
-
-# test -e overlays/modules/kernel-modules.tar.gz && rm overlays/modules/kernel-modules.tar.gz
-
-# # image and dtb
-# cp rk3566-pinenote-v1.2.dtb ../overlays/boot/
-# cp Image ../overlays/boot/
-# cp modules.tar.gz ../overlays/modules/kernel-modules.tar.gz
-
-cp linux-image*no_compression.deb ../overlays/kernel/
-
-cd ..
-rm -r tmp_kernel
+cd overlays/kernel/
+wget https://github.com/m-weigand/linux/releases/download/v20230603_test1/linux-image-6.3.0-pinenote-202306031741-gac9fb7fb7448_6.3.0-gac9fb7fb7448-1_arm64_no_compression.deb
+cd ../../
