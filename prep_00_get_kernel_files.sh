09f3b712f920c07aba06ce06b9f178b72b443407
Maximilian Weigand <mweigand@mweigand.net>
2023-06-05 10:31:12 +0200

    install newest kernel 6.3-v2 in the image by default

--- prep_00_get_kernel_files.sh
+++ prep_00_get_kernel_files.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
 cd overlays/kernel/
-wget https://github.com/m-weigand/linux/releases/download/v20230603_test1/linux-image-6.3.0-pinenote-202306031741-gac9fb7fb7448_6.3.0-gac9fb7fb7448-1_arm64_no_compression.deb
+rm *.deb
+# 6.3_v2 kernel
+wget https://github.com/m-weigand/linux/releases/download/v20230605/linux-image-6.3.0-pinenote-202306050736-g0f64a7bedfee_6.3.0-g0f64a7bedfee-1_arm64_no_compression.deb
+wget https://github.com/m-weigand/linux/releases/download/v20230605/linux-headers-6.3.0-pinenote-202306050736-g0f64a7bedfee_6.3.0-g0f64a7bedfee-1_arm64.deb
 cd ../../
