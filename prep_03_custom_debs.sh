6787da051643fa3da3e277d1e18ada28d5d7337b
Maximilian Weigand <mweigand@mweigand.net>
2023-07-08 14:33:41 +0200

    deactivate manual installation of mutter and mesa packages - they should be installed using the repo

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -2,34 +2,31 @@
 
 pwd=$PWD
 
-cd overlays/custom_debs/
-test -d mutter && rm -r mutter
-mkdir mutter && cd mutter
+# cd overlays/custom_debs/
+# test -d mutter && rm -r mutter
+# mkdir mutter && cd mutter
 
-# wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20221231/mutter.zip
-# wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230317/mutter.zip
-# wget https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230428/mutter.zip
-wget -nv https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230517/mutter.zip
-unzip mutter.zip *.deb
-rm *dbgsym*.deb
-rm *dev*.deb
-rm *tests*.deb
-rm mutter.zip
-cd ${pwd}
+# wget -nv https://github.com/m-weigand/pinenote_debian_mutter/releases/download/v20230517/mutter.zip
+# unzip mutter.zip *.deb
+# rm *dbgsym*.deb
+# rm *dev*.deb
+# rm *tests*.deb
+# rm mutter.zip
+# cd ${pwd}
 
-cd overlays/custom_debs/
-test -d mesa && rm -r mesa
-mkdir mesa && cd mesa
+# cd overlays/custom_debs/
+# test -d mesa && rm -r mesa
+# mkdir mesa && cd mesa
 
-# wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v22.3.3-1_v1/mesa_arm64_pinenote.zip
-wget -nv https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v20230517/mesa_arm64_pinenote.zip
-unzip mesa_arm64_pinenote.zip *.deb
-rm *dbgsym*.deb
-rm *dev*.deb
-rm libd3dadapter9-mesa*.deb
-rm mesa-opencl-icd*_arm64.deb
-rm mesa_arm64_pinenote.zip
-cd ${pwd}
+# # wget https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v22.3.3-1_v1/mesa_arm64_pinenote.zip
+# wget -nv https://github.com/m-weigand/pinenote_debian_mesa/releases/download/v20230517/mesa_arm64_pinenote.zip
+# unzip mesa_arm64_pinenote.zip *.deb
+# rm *dbgsym*.deb
+# rm *dev*.deb
+# rm libd3dadapter9-mesa*.deb
+# rm mesa-opencl-icd*_arm64.deb
+# rm mesa_arm64_pinenote.zip
+# cd ${pwd}
 
 # koreader
 cd overlays/custom_debs/
