b241d86056e00bedfd723294d3079c92a9b84b38
Maximilian Weigand <mweigand@mweigand.net>
2024-02-27 12:26:32 +0100

    fix uboot files

--- overlays/root/uboot/install_fast_1560mhz_uboot.sh
+++ overlays/root/uboot/install_fast_1560mhz_uboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /root/uboot/
-dd if=u-boot-1560mhz/u-boot-pinenote/idblock.bin of=/dev/mmcblk0 seek=64
-dd if=u-boot-1560mhz/u-boot-pinenote/uboot.img of=/dev/mmcblk0 seek=16384
+dd if=u-boot-1560mhz/u-boot-pinenote/idblock_1560mhz.bin of=/dev/mmcblk0 seek=64
+dd if=u-boot-1560mhz/u-boot-pinenote/uboot_1560mhz.img of=/dev/mmcblk0 seek=16384
 echo "If not errors were reported, then the 1560 MHz u-boot/ram-blob was installed"
 echo "Please reboot"
