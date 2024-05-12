aad3bbe8b5cd383ff07f022f9828128dabe326d1
Maximilian Weigand <mweigand@mweigand.net>
2024-05-12 12:10:39 +0200

    update pinenote dbus service

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -23,7 +23,8 @@ cd "${pwd}"
 # pinenote dbus service
 cd overlays/custom_debs
 test -e pinenote-dbus-service*.deb && rm pinenote-dbus-service*.deb
-wget -nv https://github.com/PNDeb/pinenote_dbus_service/releases/download/v0.2.1-dev/pinenote-dbus-service_0.2.1-dev-1_arm64.deb
+# wget -nv https://github.com/PNDeb/pinenote_dbus_service/releases/download/v0.2.1-dev/pinenote-dbus-service_0.2.1-dev-1_arm64.deb
+wget -nv https://github.com/PNDeb/pinenote_dbus_service/releases/download/v0.2.2-dev1/pinenote-dbus-service_0.2.2.dev1-1_arm64.deb
 # wget -nv https://github.com/PNDeb/pinenote_dbus_service/releases/download/v20231208/pinenote-dbus-service_0.2.0-dev-1_arm64.deb
 # wget -nv https://github.com/m-weigand/pinenote_dbus_service/releases/download/v20221207/pinenote_dbus_service_0.1.0_arm64.deb
 cd "${pwd}"
