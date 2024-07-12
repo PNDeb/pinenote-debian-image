ae796099964e992fb1d814109a1fc93a9856b4d0
Maximilian Weigand <mweigand@mweigand.net>
2024-07-12 21:21:15 +0200

    For now, use our fork of the PNEink GNOME theme

--- prep_05_get_external_files.sh
+++ prep_05_get_external_files.sh
@@ -4,5 +4,6 @@
 # get gnome theme
 cd overlays/gnome_theme
 test -d PNEink && rm -rf PNEink
-git clone https://github.com/MichiMolle/PNEink.git
+# git clone https://github.com/MichiMolle/PNEink.git
+git clone https://github.com/m-weigand/PNEink.git
 cd ../../
