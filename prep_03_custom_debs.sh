4f51942ce33dac920e86bc25abda2c63ae39e56d
Maximilian Weigand <mweigand@mweigand.net>
2025-01-22 15:15:14 +0800

    update koreader to 2024.11

--- prep_03_custom_debs.sh
+++ prep_03_custom_debs.sh
@@ -5,7 +5,8 @@ pwd=$PWD
 cd overlays/custom_debs/
 test -e koreader-2023.08-arm64.deb && rm koreader*.deb
 # wget -nv https://github.com/koreader/koreader/releases/download/v2024.04/koreader-2024.04-arm64.deb
-wget -nv https://github.com/koreader/koreader/releases/download/v2024.07/koreader-2024.07-arm64.deb
+# wget -nv https://github.com/koreader/koreader/releases/download/v2024.07/koreader-2024.07-arm64.deb
+wget -nv https://github.com/koreader/koreader/releases/download/v2024.11/koreader-2024.11-arm64.deb
 cd ${pwd}
 
 # evsieve
